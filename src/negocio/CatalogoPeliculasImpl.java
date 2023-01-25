1fb57caa0912fac153af0f679a7e544de21cf39d
AJ-Mareco <loarregloyomismo@gmail.com>
2023-01-25 18:46:51 -0300

    se crearon metodos del catalogoPeliculasImpl

--- src/negocio/CatalogoPeliculasImpl.java
+++ src/negocio/CatalogoPeliculasImpl.java
@@ -1,29 +1,70 @@
-
 package negocio;
 
-public class CatalogoPeliculasImpl implements ICatalogoPeliculas{
+import datos.AccesoDatosImpl;
+import datos.IAccesoDatos;
+import dominio.Pelicula;
+import excepciones.*;
+
+
+public class CatalogoPeliculasImpl implements ICatalogoPeliculas {
+
+    private final IAccesoDatos datos;
 
     public CatalogoPeliculasImpl() {
-    }
-    
-    @Override
-    public void agrgarPelicula(String nombrePelicula, String nombreArchivo) {
-        
+        this.datos = new AccesoDatosImpl();
     }
 
     @Override
-    public void listarPeliculas(String nombreArchivo) {
-        
+    public void agregarPelicula(String nombrePelicula) {
+        Pelicula pelicula = new Pelicula(nombrePelicula);
+        boolean anexar = false;
+        try {
+            anexar = datos.existe(NOMBRE_RECURSO);
+            datos.escribir(pelicula, NOMBRE_RECURSO, anexar);
+        } catch (AccesoDatosEx ex) {
+            System.out.println("Error de acceso a datos ");
+            ex.printStackTrace(System.out);
+        }
     }
 
     @Override
-    public void buscarPelicula(String nombreArchivo, String buscar) {
+    public void listarPeliculas() {
+        try {
+            var peliculas = this.datos.listar(NOMBRE_RECURSO);
+            for (var pelicula : peliculas) {
+                System.out.println("pelicula = " + pelicula);
+            }
+        } catch (LecturaDatosEx ex) {
+            System.out.println("error de acceso de datos");
+            ex.printStackTrace(System.out);
+        }
 
     }
 
     @Override
-    public void iniciarArchivo(String nombreArchivo) {
- 
+    public void buscarPelicula(String buscar) {
+        String resultado = null;
+        try {
+            resultado = this.datos.buscar(NOMBRE_RECURSO, buscar);
+        } catch (AccesoDatosEx ex) {
+            System.out.println("error de acceso de datos");
+            ex.printStackTrace(System.out);
+        }
+        System.out.println("resultado = " + resultado);
     }
-    
+
+    @Override
+    public void iniciarCatalogoPeliculas() {
+        try {
+            if (this.datos.existe(NOMBRE_RECURSO)) {
+                datos.borrar(NOMBRE_RECURSO);
+                datos.crear(NOMBRE_RECURSO);
+            } else {
+                datos.crear(NOMBRE_RECURSO);
+            }
+        } catch (AccesoDatosEx ex) {
+            System.out.println("error iniciar catalogo");
+            ex.printStackTrace(System.out);
+        }
+    }
 }
